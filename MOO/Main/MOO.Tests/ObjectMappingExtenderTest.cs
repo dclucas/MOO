d95dbc7117ea510372f3b882ce8bc076de965bfd
Diogo <diogo.lucas@gmail.com>
2011-09-20 17:39:08 -0300

    First unit tests for ObjectMappingExtender.

---
+++ MOO/Main/MOO.Tests/ObjectMappingExtenderTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moo;
+
+namespace Moo.Tests
+{
+    [TestClass]
+    public class ObjectMappingExtenderTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MapTo_NullSource_Throws()
+        {
+            TestClassA source = null;
+            source.MapTo<TestClassB>(new TestClassB());
+        }
+    }
+}
