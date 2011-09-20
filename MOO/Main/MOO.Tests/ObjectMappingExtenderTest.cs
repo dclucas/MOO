9e7dad069232f45ecad1d105b9b2bc5098eb1ccf
Diogo <diogo.lucas@gmail.com>
2011-09-20 17:46:10 -0300

    First unit tests for ObjectMappingExtender.

--- MOO/Main/MOO.Tests/ObjectMappingExtenderTest.cs
+++ MOO/Main/MOO.Tests/ObjectMappingExtenderTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moo;
+using Moq;
 
 namespace Moo.Tests
 {
@@ -12,10 +13,42 @@ namespace Moo.Tests
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void MapTo_NullSource_Throws()
+        public void MapTo_NullSource_Throws_1()
         {
             TestClassA source = null;
             source.MapTo<TestClassB>(new TestClassB());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MapTo_NullSource_Throws_2()
+        {
+            TestClassA source = null;
+            source.MapTo<TestClassB>();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MapTo_NullSource_Throws_3()
+        {
+            TestClassA source = null;
+            source.MapTo<TestClassB>(new Mock<IMapper>().Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MapTo_NullSource_Throws_4()
+        {
+            TestClassA source = null;
+            source.MapTo<TestClassB>(new Mock<IMappingRepository>().Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MapTo_NullSource_Throws_5()
+        {
+            TestClassA source = null;
+            source.MapTo<TestClassB>(new TestClassB(), new Mock<IMapper>().Object);
         }
     }
 }
