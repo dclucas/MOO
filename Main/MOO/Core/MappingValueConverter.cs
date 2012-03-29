a7b93242dbf34818eed2e5375ab9f77769f5ebf9
dlucas <diogo.lucas@gmail.com>
2012-03-29 20:52:40 -0300

    Removing TFS bindings - this ship has sailed.

---
+++ Main/MOO/Core/MappingValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moo.Core
+{
+    internal class MappingValueConverter : ValueConverter
+    {
+        public override bool CanConvert(Type sourceType, Type targetType)
+        {
+            return 
+                sourceType.Assembly.Equals(targetType.Assembly) 
+                || base.CanConvert(sourceType, targetType);
+        }
+
+        public override object Convert(object sourceValue, Type targetType)
+        {
+            if (sourceValue == null)
+                return null;
+
+            var srcType = sourceValue.GetType();
+
+            if (base.CanConvert(srcType, targetType))
+                return base.Convert(sourceValue, targetType);
+
+            return Convert(sourceValue, targetType, MappingRepository.Default.ResolveMapper(srcType, targetType));
+        }
+
+        private object Convert(object sourceValue, Type targetType, IMapper mapper)
+        {
+            return mapper.Map(sourceValue);
+        }
+    }
+}
