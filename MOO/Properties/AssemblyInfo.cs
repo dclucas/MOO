778be80ba06e6e0e5337abc45d5ae1ffe1a12992
Diogo <diogo.lucas@gmail.com>
2013-04-06 01:49:05 -0300

    Changing assembly version to 0.9 and the informational version to RC 1, to prepare for RC release.

--- MOO/Properties/AssemblyInfo.cs
+++ MOO/Properties/AssemblyInfo.cs
@@ -59,7 +59,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8")]
-[assembly: AssemblyFileVersion("0.8")]
+[assembly: AssemblyVersion("0.9")]
+[assembly: AssemblyFileVersion("0.9")]
 [assembly: CLSCompliant(true)]
+[assembly: AssemblyInformationalVersion("RC 1")]
 [assembly: InternalsVisibleTo("Moo.Tests")]
